f2b8171888889d04cb6645b82cf61a4f971b2c77
student <student@nptu01.local>
2019-03-28 09:17:24 +0800

    Initial Commit

---
+++ note/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  note
+//
+//  Created by student on 2019/3/28.
+//  Copyright © 2019年 student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

87d5fd352d23034e71e5a0e62b10bbaecef993c2
Kuei <vx3012564897@gmail.com>
2020-03-18 00:32:03 +0800

    already finished project

--- note/FirstViewController.swift
+++ note/FirstViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource,UITabBarDelegate {
+    
+    var items: [String] = []
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let itemObj = UserDefaults.standard.object(forKey: "userEntry")
+        if let tempItem = itemObj as? [String] {
+            items = tempItem
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 6
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellObj = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        //cellObj.textLabel?.text = "test"
+        
+        cellObj.textLabel?.text = items[indexPath.row]
+        
+        return cellObj
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            items.remove(at: indexPath.row)
+            tableView.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "userEntry")
+        }
+    }
 }
 
--- note/SecondViewController.swift
+++ note/SecondViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textCheck: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @IBAction func saveBtn(_ sender: UIButton) {
+        var items : [String] = []
+        
+        let itemObj = UserDefaults.standard.object(forKey: "userEntry")
+        
+        
+        
+        if textField.text != ""{
+            if let tempItem = itemObj as? [String]{
+                items = tempItem
+                //加入訊息通知使用者已存入相同資訊
+               var index = -1
+                for str in items{
+                    index += 1
+                    if str == textField.text{
+                        textCheck.text = "已存有相同內容"
+                        items.remove(at: index)
+                        print("/n \(index)")
+                        break
+                    }else{
+                        textCheck.text = "已存入新內容"
+                    }
+                }
+                items.append(textField.text!)
+                print(items)
+                
+            }else{
+                items = [textField.text!]
+            }
+            
+            UserDefaults.standard.set(items,forKey:"userEntry")
+            textField.text = ""
+        }
+        
 
-
+    }
 }
 
